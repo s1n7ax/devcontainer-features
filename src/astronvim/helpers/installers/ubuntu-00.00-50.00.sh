8b3d7d73c3db313b3deaec73b3fd289ee1e6eef7
s1n7ax <srineshnisala@gmail.com>
2023-10-09 01:35:27 +0530

    plugins are not installed on features

--- src/astronvim/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/astronvim/helpers/installers/ubuntu-00.00-50.00.sh
@@ -16,20 +16,4 @@ rm "$ASTRONVIM_VERSION.tar.gz"
 mkdir -p ~/.config
 mv "AstroNvim-${ASTRONVIM_VERSION#v}" ~/.config/nvim
 
-commands=("nvim" "git" "gcc" "c++")
-
-all_commands_found=true
-
-for cmd in "${commands[@]}"; do
-	if ! command -v "$cmd" &>/dev/null; then
-		all_commands_found=false
-	fi
-done
-
-if $all_commands_found; then
-	nvim --headless "+Lazy! sync" +qa
-else
-	echo "Skipping installing AstroNvim" >&2
-	echo "Not any or all requirements are met" >&2
-	echo "Following should be present: nvim git gcc c++" >&2
-fi
+nvim --headless "+Lazy! sync" +qa
