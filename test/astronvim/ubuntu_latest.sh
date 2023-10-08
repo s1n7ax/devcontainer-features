6541ce19bb008eba56aeda8a51c05614a06c119e
s1n7ax <srineshnisala@gmail.com>
2023-10-09 01:20:14 +0530

    remove plugins validations from tests

--- test/astronvim/ubuntu_latest.sh
+++ test/astronvim/ubuntu_latest.sh
@@ -7,6 +7,5 @@ set -euxo pipefail
 source dev-container-features-test-lib
 
 check "AstroNvim config is available" bash -c "ls ~/.config/nvim/lua/astronvim"
-check "AstroNvim plugins are insalled" bash -c "ls ~/.local/share/nvim/lazy"
 
 reportResults
