3920ffaa128c60e636a51e98cbd125600173b38b
s1n7ax <18459807+s1n7ax@users.noreply.github.com>
2023-10-08 18:16:53 +0000

    fix(test): failing due to unsupported os (#8)

--- test/neovim/neovim_alpine.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eux
-
-# test lib cannot be used for alpine because it only support bash and alpine only got sh
-# source dev-container-features-test-lib
-
-nvim --version | grep NVIM
-nvim --version | grep 'NVIM v0.9.2'
