bfface0b11aa6df0d556a63fec612c12a4808bf2
s1n7ax <srineshnisala@gmail.com>
2023-10-09 01:13:24 +0530

    fix: astronvim plugins are not installed

--- test/astronvim/ubuntu_latest.sh
+++ test/astronvim/ubuntu_latest.sh
@@ -7,5 +7,6 @@ set -euxo pipefail
 source dev-container-features-test-lib
 
 check "AstroNvim config is available" bash -c "ls ~/.config/nvim/lua/astronvim"
+check "AstroNvim plugins are insalled" bash -c "ls ~/.local/share/nvim/lazy"
 
 reportResults
