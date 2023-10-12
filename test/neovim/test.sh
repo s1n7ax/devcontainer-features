1b162ce651f832032dec9d2d649f4342a5ba17c0
s1n7ax <srineshnisala@gmail.com>
2023-10-12 17:08:18 +0530

    fix(test): neovim tests

--- test/neovim/test.sh
+++ test/neovim/test.sh
@@ -4,10 +4,9 @@ set -euo pipefail
 
 # This refers scenarios.json
 
-set -euxo pipefail
-
 source dev-container-features-test-lib
 
-check "Neovim executable should be available" bash -c "nvim --version | grep 'NVIM v0.9.4'"
+check "Neovim executable should be available" bash -c "nvim --version"
+check "Neovim version should be nightly" bash -c "nvim --version | grep 'NVIM v0.9.4'"
 
 reportResults
