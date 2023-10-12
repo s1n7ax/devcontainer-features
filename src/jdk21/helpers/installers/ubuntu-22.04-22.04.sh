f3b03328bc41efef9269369be8ba52012d837d07
s1n7ax <srineshnisala@gmail.com>
2023-10-12 16:50:07 +0530

    feat: add jdtls to the jdk21 installation

--- src/jdk21/helpers/installers/ubuntu-22.04-22.04.sh
+++ src/jdk21/helpers/installers/ubuntu-22.04-22.04.sh
@@ -6,10 +6,22 @@ TEMP="$(mktemp -d)"
 cd "$TEMP"
 
 apt update
-apt install -y wget
-wget -O jdk.tar.gz https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21%2B35/OpenJDK21U-jdk_x64_linux_hotspot_21_35.tar.gz
-tar xf jdk.tar.gz
-mv jdk-21+35 /opt/jdk
+apt install -y axel
 
-export PATH="/opt/jdk/bin:$PATH"
-export JAVA_HOME="/opt/jdk"
+# JDK
+axel \
+	--num-connections=5 \
+	--output=jdk.tar.gz \
+	https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21%2B35/OpenJDK21U-jdk_x64_linux_hotspot_21_35.tar.gz
+tar xf jdk.tar.gz
+mv jdk-21+35 /opt/jdk21
+
+# JDTLS
+if [ "$INSTALL_JDTLS" = "true" ]; then
+	axel \
+		--num-connections=5 \
+		--output=jdtls.tar.gz \
+		https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.28.0/jdt-language-server-1.28.0-202309281329.tar.gz
+	mkdir /opt/jdtls
+	tar xf jdtls.tar.gz -C /opt/jdtls
+fi
