f7a87805e39ddba338fd68aa17675f7d6c6937da
s1n7ax <srineshnisala@gmail.com>
2023-10-12 17:38:38 +0530

    fix: jdk is failing due to SSL error

--- src/jdk21/helpers/installers/ubuntu-22.04-22.04.sh
+++ src/jdk21/helpers/installers/ubuntu-22.04-22.04.sh
@@ -12,6 +12,7 @@ apt install -y axel
 axel \
 	--num-connections=5 \
 	--output=jdk.tar.gz \
+	--insecure \
 	https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21%2B35/OpenJDK21U-jdk_x64_linux_hotspot_21_35.tar.gz
 tar xf jdk.tar.gz
 mv jdk-21+35 /opt/jdk21
@@ -21,6 +22,7 @@ if [ "$INSTALL_JDTLS" = "true" ]; then
 	axel \
 		--num-connections=5 \
 		--output=jdtls.tar.gz \
+		--insecure \
 		https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.28.0/jdt-language-server-1.28.0-202309281329.tar.gz
 	mkdir /opt/jdtls
 	tar xf jdtls.tar.gz -C /opt/jdtls
