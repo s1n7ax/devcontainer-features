bdc48214f1c902b95823c57e6e0e3f319a7a2a1f
s1n7ax <srineshnisala@gmail.com>
2023-10-12 17:16:48 +0530

    refactor(test): update jdk21 test

--- test/jdk21/ubuntu-latest.sh
+++ test/jdk21/ubuntu-latest.sh
@@ -8,6 +8,7 @@ set -euxo pipefail
 
 source dev-container-features-test-lib
 
-check "Java executable should be available" bash -c "java --version | grep 'openjdk 21'"
+check "Java executable should be available" bash -c "java --version"
+check "Java version should be jdk 21" bash -c "java --version | grep 'openjdk 21'"
 
 reportResults
