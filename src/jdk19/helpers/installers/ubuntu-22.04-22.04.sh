c195dcc044075fff10d6172ed545414955370307
s1n7ax <srineshnisala@gmail.com>
2023-10-16 20:13:04 +0530

    debug: check the opt path

--- src/jdk19/helpers/installers/ubuntu-22.04-22.04.sh
+++ src/jdk19/helpers/installers/ubuntu-22.04-22.04.sh
@@ -16,3 +16,4 @@ axel \
 	https://github.com/adoptium/temurin19-binaries/releases/download/jdk-19.0.2%2B7/OpenJDK19U-jdk_aarch64_linux_hotspot_19.0.2_7.tar.gz
 tar xf jdk.tar.gz
 mv jdk-19.0.2+7 /opt/jdk19
+ls /opt/jdk19
