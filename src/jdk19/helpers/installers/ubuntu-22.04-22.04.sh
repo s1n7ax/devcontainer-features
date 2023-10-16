3b104f99a57b1af56acd1efc59ffaf6e4a5194a4
s1n7ax <srineshnisala@gmail.com>
2023-10-16 21:20:16 +0530

    chore: code clean up

--- src/jdk19/helpers/installers/ubuntu-22.04-22.04.sh
+++ src/jdk19/helpers/installers/ubuntu-22.04-22.04.sh
@@ -16,5 +16,3 @@ axel \
 	https://github.com/adoptium/temurin19-binaries/releases/download/jdk-19.0.2%2B7/OpenJDK19U-jdk_x64_linux_hotspot_19.0.2_7.tar.gz
 tar xf jdk.tar.gz
 mv jdk-19.0.2+7 /opt/jdk19
-ls /opt/jdk19
-ls /opt/jdk19/bin
