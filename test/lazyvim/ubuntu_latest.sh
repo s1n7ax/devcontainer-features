d1a154c4f6f6c9ee2144e4eb2a0675667c1ba8ea
s1n7ax <srineshnisala@gmail.com>
2024-05-27 01:14:57 +0530

    chore(test): adding tests for lazyvim

---
+++ test/lazyvim/ubuntu_latest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This refers scenarios.json
+
+set -euxo pipefail
+
+source dev-container-features-test-lib
+
+check "AstroNvim config is available" bash -c "ls ~/.config/nvim/lua"
+
+reportResults
