b78e116edfc845554b7ad3b151cde23fe6eb9fdb
s1n7ax <srineshnisala@gmail.com>
2025-02-17 16:24:57 +0530

    chore: stable

--- test/neovim/test.sh
+++ test/neovim/test.sh
@@ -7,6 +7,6 @@ set -euo pipefail
 source dev-container-features-test-lib
 
 check "Neovim executable should be available" bash -c "nvim --version"
-check "Neovim version should be latest stable version" bash -c "nvim --version | grep 'NVIM v0.10.2'"
+check "Neovim version should be latest stable version" bash -c "nvim --version | grep 'NVIM v0.10.4'"
 
 reportResults
