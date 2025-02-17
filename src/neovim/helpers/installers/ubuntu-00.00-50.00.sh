9a24a79909793ca9590e907eebed6f9d3468bd59
s1n7ax <srineshnisala@gmail.com>
2025-02-17 16:12:57 +0530

    test

--- src/neovim/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/neovim/helpers/installers/ubuntu-00.00-50.00.sh
@@ -2,32 +2,37 @@
 
 set -eux
 
+#--------------------------------------------------------------------#
+#                               CONST                                #
+#--------------------------------------------------------------------#
+EXTRACTED_DIR_NAME="nvim-linux-x86_64"
+COMPRESSED_FILE_NAME="${EXTRACTED_DIR_NAME}.tar.gz"
 TEMP_DIR="$(mktemp -d)"
+
 cd "$TEMP_DIR"
 
 apt update
 apt install -y wget
 
-wget "https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim-linux-x86_64.tar.gz"
-tar xf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
+wget "https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/$COMPRESSED_FILE_NAME"
+tar xf "$COMPRESSED_FILE_NAME"
+rm "$COMPRESSED_FILE_NAME"
 
 # move the executable
-mv --force nvim-linux64/bin/nvim /usr/local/bin
+mv --force $EXTRACTED_DIR_NAME/bin/nvim /usr/local/bin
 
 # --symbolic & --force flags does not exist in alpine & busybox
 ln -s -f /usr/local/bin/nvim /usr/bin
 
 # copy share files
-cp --recursive --update --verbose nvim-linux64/share/* /usr/local/share
-rm -rf nvim-linux64/share
+cp --recursive --update --verbose ${EXTRACTED_DIR_NAME}/share/* /usr/local/share
 
 # copy libs
-cp --recursive --update --verbose nvim-linux64/lib/* /usr/local/lib
-rm -rf nvim-linux64/lib
+cp --recursive --update --verbose ${EXTRACTED_DIR_NAME}/lib/* /usr/local/lib
 
 # copy man pages
-if [ -d nvim-linux64/man ]; then
-  cp --recursive --update --verbose nvim-linux64/man/* /usr/local/man
-  rm -rf nvim-linux64/man
+if [ -d ${EXTRACTED_DIR_NAME}/man ]; then
+  cp --recursive --update --verbose ${EXTRACTED_DIR_NAME}/man/* /usr/local/man
 fi
+
+rm --recursive --force "$TEMP_DIR"
